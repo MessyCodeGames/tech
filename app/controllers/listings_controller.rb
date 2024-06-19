d899a3ba065e16e2fe5b44cc8695d24107c5de06
robin chaux <robinvchaux@gmail.com>
2024-06-19 12:28:28 +0200

    Finished the basic API

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -10,15 +10,34 @@ class ListingsController < ApplicationController
   end
 
   def new
+    @list = Listing.new
   end
 
   def create
+    @list = Listing.new(num_room: params[:num_room])
+    if @list.save
+      render json: @list
+    else
+      render json: @list.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @list = Listing.find(params[:id])
+    if @list.destroy
+      render json: { message: 'Listing successfully deleted' }, status: 200
+    else
+      render json: @list.errors.full_messages, status: 422
+    end
   end
 
   def update
+    @list = Listing.find(params[:id])
+    if @list.update(num_room: params[:num_room])
+      render json: @list
+    else
+      render json: @list.errors.full_messages, status: 422
+    end
   end
 
 
